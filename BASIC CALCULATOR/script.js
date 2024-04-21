15b3250e265b08624932c256fce62c99e1b18bca
sandipan287 <161804554+sandipan287@users.noreply.github.com>
2024-04-21 13:52:44 +0530

    Update script.js

--- BASIC CALCULATOR/script.js
+++ BASIC CALCULATOR/script.js
@@ -24,16 +24,7 @@ window.onload = function() {
       
     function entries(s) {
         input.value += s;
-        /* button1: s = "1"
-        input.value = undefined
-        entries("1")
-        input.value =+ s
-        input.value = input.value + s = undefined + "1" = "1"
-        button2 : s = "2"
-        input.value ="1"
-        entries("2")
-        input.value = input.value + s = "1" + "2" ="12"
-        */
+        
     }
 
     function calculate() {
